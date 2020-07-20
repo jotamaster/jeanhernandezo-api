234e60c1b6fff60f5e5a3b63c458a1354dc8356a
jotamaster <j3anhernandez@gmail.com>
2020-07-20 17:57:03 -0400

    add ligin an register flow

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post('login', 'Api\AuthController@login');
+Route::post('register', 'Api\AuthController@register');
+
+Route::get('unauthorized',  function(Request $request){
+    return response()->json(['message'=>'unauthorized'],401);
+})->name('unauthorized');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['middleware' => 'auth:api'], function(){
+    Route::get('user-detail', 'Api\AuthController@userDetail');
 });
