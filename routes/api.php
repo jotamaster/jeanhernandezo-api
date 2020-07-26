3101ee94457fa565c30c36a978b001c0785640b6
jotamaster <j3anhernandez@gmail.com>
2020-07-26 16:06:43 -0400

    add auth flow

--- routes/api.php
+++ routes/api.php
@@ -15,17 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::post('login', 'Api\AuthController@login');
-Route::post('register', 'Api\AuthController@register');
+// Route::post('login', 'Api\AuthController@login');
+// Route::post('register', 'Api\AuthController@register');
 
-Route::get('unauthorized',  function(Request $request){
-    return response()->json(['message'=>'unauthorized'],401);
-})->name('unauthorized');
+// Route::get('unauthorized',  function(Request $request){
+//     return response()->json(['message'=>'unauthorized'],401);
+// })->name('unauthorized');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
-Route::group(['middleware' => 'auth:api'], function(){
-    Route::get('user-detail', 'Api\AuthController@userDetail');
-});
+Route::get('/migrate', 'MigrateController@index');
+Route::post('/auth/login', 'AuthController@login');
+Route::post('/auth/register', 'AuthController@register');
+Route::get('/auth/me', 'AuthController@me');
+
+// Route::group(['middleware' => 'auth:api'], function(){
+//     Route::get('user-detail', 'Api\AuthController@userDetail');
+// });
