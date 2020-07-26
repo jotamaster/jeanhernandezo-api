eca60db33a67e9ae7fdb5fc9c0e552066e0ccfb4
jotamaster <j3anhernandez@gmail.com>
2020-07-26 17:15:59 -0400

    add login

--- routes/api.php
+++ routes/api.php
@@ -18,9 +18,9 @@ use Illuminate\Support\Facades\Route;
 // Route::post('login', 'Api\AuthController@login');
 // Route::post('register', 'Api\AuthController@register');
 
-// Route::get('unauthorized',  function(Request $request){
-//     return response()->json(['message'=>'unauthorized'],401);
-// })->name('unauthorized');
+Route::get('unauthorized',  function(Request $request){
+    return response()->json(['message'=>'unauthorized'],401);
+})->name('unauthorized');
 
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
